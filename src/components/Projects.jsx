e28a33cd147037ec40e0b3539981ee208d954025
Nian Liu <nianliu18@gmail.com>
2019-01-30 16:51:49 -0500

    minor

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,6 +5,13 @@ const Projects = (props) => {
   return (
     <div>
         <h1>New Start put new projects when there is time</h1>
+        <div className= 'item-a'>
+          <h1> React Lucid </h1>
+          <p>
+            Lucid is a Chrome developer tool that helps engineers debug their React/Redux, Apollo/GraphQL applications. The tool gives our users visualization of their application's component hierarchy and access to their GraphQL mutations, queries, schemas and query responses.
+          </p>
+          <a href='https://github.com/Gossamer-React/Lucid'> Click to Github </a>
+        </div>
     </div>
   );
 }
